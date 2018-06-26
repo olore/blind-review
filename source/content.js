863eb8586c174d621bf39e2ee01f40ae40135659
Brian Olore <brian@olore.net>
2018-06-26 07:30:18 -0400

    2 space indent like a human

--- source/content.js
+++ source/content.js
@@ -2,62 +2,66 @@ import elementReady from 'element-ready';
 import domLoaded from 'dom-loaded';
 
 async function init() {
-    await safeElementReady('body');
-    document.addEventListener('pjax:end', doWork);  // doWork after github page navigation is complete
+  await safeElementReady('body');
+  document.addEventListener('pjax:end', doWork);  // doWork after github page navigation is complete
 
-    await domLoaded;
-    await Promise.resolve();
+  await domLoaded;
+  await Promise.resolve();
 
-    doWork(); // doWork after first github page is complete
+  doWork(); // doWork after first github page is complete
 }
 
 function doWork() {
-    let uri = window.location.pathname;
-    if (!uri.match(/\/pull\//) && !uri.match(/\/pulls$/)) { // skip if we aren't on a PR page
-        return;
-    }
-    let block = "&block;&block;&block;"
-    let blackImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=";
-
-    let imageSelectors = [
-        'img.avatar',
-        'img.from-avatar',
-        'a.avatar > img',
-    ];
-    let authorSelectors = [
-        'a.author',
-        'a.assignee', 
-        '.opened-by > a.muted-link',
-        'a.commit-author',
-    ];
-
-    let imageNodes = Array.from(document.querySelectorAll(imageSelectors.join(', ')));
-    let authorNodes = Array.from(document.querySelectorAll(authorSelectors.join(', ')));
-
-
-    console.log('imageNodes', imageNodes.length);
-    console.log('authorNodes', authorNodes.length);
-
-    imageNodes.forEach((img, i) => {
-        img.src = blackImage;
-    });
-
-    authorNodes.forEach((a, i) => {
-        a.innerHTML = block;
-    });
+  let uri = window.location.pathname;
+  if (!uri.match(/\/pull\//) && !uri.match(/\/pulls$/)) { // skip if we aren't on a PR page
+    return;
+  }
+  obfuscate();
 }
 
+function obfuscate() {
+  let block = "&block;&block;&block;"
+  let blackImage = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=";
+
+  let imageSelectors = [
+    'img.avatar',
+    'img.from-avatar',
+    'a.avatar > img',
+  ];
+  let authorSelectors = [
+    'a.author',
+    'a.assignee',
+    '.opened-by > a.muted-link',
+    'a.commit-author',
+  ];
+
+  let imageNodes = Array.from(document.querySelectorAll(imageSelectors.join(', ')));
+  let authorNodes = Array.from(document.querySelectorAll(authorSelectors.join(', ')));
+
+  // console.log('imageNodes', imageNodes.length);
+  // console.log('authorNodes', authorNodes.length);
+
+  imageNodes.forEach((img, i) => {
+    img.src = blackImage;
+  });
+
+  authorNodes.forEach((a, i) => {
+    a.innerHTML = block;
+  });
+}
+
+// from https://github.com/sindresorhus/refined-github
 /**
  * Automatically stops checking for an element to appear once the DOM is ready.
  */
 export const safeElementReady = selector => {
-	const waiting = elementReady(selector);
+  const waiting = elementReady(selector);
 
-	// Don't check ad-infinitum
-	domLoaded.then(() => requestAnimationFrame(() => waiting.cancel()));
+  // Don't check ad-infinitum
+  domLoaded.then(() => requestAnimationFrame(() => waiting.cancel()));
 
-	// If cancelled, return null like a regular select() would
-	return waiting.catch(() => null);
+  // If cancelled, return null like a regular select() would
+  return waiting.catch(() => null);
 };
 
 init();
